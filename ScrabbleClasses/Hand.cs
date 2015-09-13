f7ec62eb214a42f7b774b3737c9c3cb397bda949
Alexander Landmesser <alexander@land-server.de>
2015-09-13 16:34:27 +0200

    added hand.cs

---
+++ ScrabbleClasses/Hand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble
+{
+    [Serializable]
+    public class Hand
+    {
+        private List<Stone> hand = new List<Stone>();
+
+        public void addStones(Stone[] stones)
+        {
+            if (hand.Count + stones.Length < 8)
+            {
+                foreach (Stone s in stones)
+                {
+                    hand.Add(s);
+                }
+            }
+        }
+
+        public Stone removeStone(char c)
+        {
+            Stone ret = hand.Find(delegate(Stone s){return s.letter == c;});
+            hand.Remove(ret);
+            return ret;
+        }
+
+        public bool hasWord(String s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s.Count(x => x == s[i]) > hand.Count(x => x.letter == s[i])){
+                    Log.log("hand","false word",4);
+                    return false;
+                }
+            }
+                Log.log("hand","ok word",4);
+                return true;
+        }
+
+        public void print()
+        {
+            foreach (Stone s in hand)
+            {
+                Console.Out.Write(s.letter + ", ");
+             
+            }
+            Console.Out.WriteLine();
+        }
+    }
+}
